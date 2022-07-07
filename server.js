2f971d70263f3b6a045f3e03bdbf110b5c337056
Pedro Freddi <pdafreddi@gmail.com>
2022-07-07 15:55:09 -0300

    Setup express-session for session management

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
-require('dotenv').config();
-const express = require('express');
+// Required modules
+require("dotenv").config();
+const express = require("express");
+const expressSession = require("express-session");
+
+// App variables
+const PORT = process.env.PORT || 8000
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('Test path');
+// Session configuration
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 3600000,
+    secure: false,
+    sameSite: 'strict'
+  },
+  resave: false,
+  saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+  // Serve cookies only over HTTPS in production
+  session.cookie.secure = true;
+}
+
+// App configuration
+app.use(expressSession(session));
+
+// Routes
+app.get("/", (req, res, next) => {
+  res.send("Test path");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on PORT ${process.env.PORT}`)
+// App start
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
