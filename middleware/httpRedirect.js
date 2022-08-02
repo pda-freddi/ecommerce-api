153270e652f9bea61402ffd028e661b949b9f0ac
Pedro Freddi <pdafreddi@gmail.com>
2022-08-02 09:43:22 -0300

    Fix infinite redirect loop

--- middleware/httpRedirect.js
+++ middleware/httpRedirect.js
@@ -1,7 +1,7 @@
 // This middlewire redirects HTTP requests to enforce the use of HTTPS
 const httpRedirect = (req, res, next) => {
   if (req.secure || req.headers["x-forwaded-proto"] === "https") {
-    next();
+    return next();
   };
   res.redirect("https://" + req.hostname + req.url);
 };
