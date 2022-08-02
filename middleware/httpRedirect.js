db6453c16aa15a296f3f30b3c2e147881cc52d7d
Pedro Freddi <pdafreddi@gmail.com>
2022-08-02 10:15:46 -0300

    Improve HTTP redirect middleware logic

--- middleware/httpRedirect.js
+++ middleware/httpRedirect.js
@@ -1,8 +1,13 @@
+const generateError = require("../helpers/generateError.js");
+
 // This middlewire redirects HTTP requests to enforce the use of HTTPS
 const httpRedirect = (req, res, next) => {
   if (req.secure || req.headers["x-forwaded-proto"] === "https") {
     return next();
   };
+  if (req.method !== "GET") {
+    return next(generateError(400, "Request must be made over HTTPS."));
+  }
   res.redirect("https://" + req.hostname + req.url);
 };
 
