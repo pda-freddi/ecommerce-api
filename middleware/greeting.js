3f3714ba9155d3fd0dd147f2edcbfacf20de36d7
Pedro Freddi <pdafreddi@gmail.com>
2022-11-16 11:57:05 -0300

    Fix link to API docs on homepage

--- middleware/greeting.js
+++ middleware/greeting.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const greeting = (req, res) => {
-  res.send("Welcome! Check the API documentation at: https://ecommerce-api-pdafr.herokuapp.com/api/docs");
+  res.send("Welcome! Check the API documentation at: https://ecommerce-api-pdafr.uc.r.appspot.com/api/docs");
 };
 
 module.exports = greeting;
